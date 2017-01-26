54f6c9e73dcf28ca72af111d89910b78436e8713
Jonathan Richards <joniff@hotmail.com>
2017-01-26 02:28:58 +0000

    Added debug mode

--- src/Terratype.TestSite/App_Plugins/Terratype/1.0.0/scripts/terratype.js
+++ src/Terratype.TestSite/App_Plugins/Terratype/1.0.0/scripts/terratype.js
@@ -1,4 +1,59 @@
 ﻿(function (root) {
+
+    angular.module('umbraco').directive('jsonText', function () {
+        return {
+            restrict: 'A', // only activate on element attribute
+            require: 'ngModel', // get a hold of NgModelController
+            link: function (scope, element, attrs, ngModelCtrl) {
+
+                var lastValid;
+
+                // push() if faster than unshift(), and avail. in IE8 and earlier (unshift isn't)
+                ngModelCtrl.$parsers.push(fromUser);
+                ngModelCtrl.$formatters.push(toUser);
+
+                // clear any invalid changes on blur
+                element.bind('blur', function () {
+                    element.val(toUser(scope.$eval(attrs.ngModel)));
+                });
+
+                // $watch(attrs.ngModel) wouldn't work if this directive created a new scope;
+                // see http://stackoverflow.com/questions/14693052/watch-ngmodel-from-inside-directive-using-isolate-scope how to do it then
+                scope.$watch(attrs.ngModel, function (newValue, oldValue) {
+                    lastValid = lastValid || newValue;
+
+                    if (newValue != oldValue) {
+                        ngModelCtrl.$setViewValue(toUser(newValue));
+
+                        // TODO avoid this causing the focus of the input to be lost..
+                        ngModelCtrl.$render();
+                    }
+                }, true); // MUST use objectEquality (true) here, for some reason..
+
+                function fromUser(text) {
+                    // Beware: trim() is not available in old browsers
+                    if (!text || text.trim() === '') {
+                        return {};
+                    } else {
+                        try {
+                            lastValid = angular.fromJson(text);
+                            ngModelCtrl.$setValidity('invalidJson', true);
+                        } catch (e) {
+                            ngModelCtrl.$setValidity('invalidJson', false);
+                        }
+                        return lastValid;
+                    }
+                }
+
+                function toUser(object) {
+                    // better than JSON.stringify(), because it formats + filters $$hashKey etc.
+                    return angular.toJson(object, true);
+                }
+            }
+        };
+    });
+
+
     root.terratypeProvider = {};
     angular.module('umbraco').controller('terratype', ['$scope', '$timeout', '$http', '$injector', function ($scope, $timeout, $http, $injector) {
         $scope.identifier = $scope.$id;
@@ -23,10 +78,10 @@
                     value.script = $scope.urlRoot + 'scripts/' + value.id + '/' + value.id + '.js';
                 });
                 if ($scope.model.value) {
-                    if (model.provider && model.provider.id != null) {
-                        $scope.setProvider(model.provider.id);
+                    if ($scope.model.value.provider && $scope.model.value.provider.id != null) {
+                        $scope.setProvider($scope.model.value.provider.id);
                     }
-                    if (model.icon && model.icon.id) {
+                    if ($scope.model.value.icon && $scope.model.value.icon.id) {
                         $scope.iconPredefineChangeInternal($scope.model.value.icon.id);
                     }
                 }
@@ -557,69 +612,4 @@
             $scope.loading = false;
         }
     }]);
-
-
-    //  Pollyfill indexOf
-    if (!Array.prototype.indexOf) {
-        Array.prototype.indexOf = function (searchElement, fromIndex) {
-            var k;
-
-            // 1. Let O be the result of calling ToObject passing
-            //    the this value as the argument.
-            if (this == null) {
-                throw new TypeError('"this" is null or not defined');
-            }
-
-            var O = Object(this);
-
-            // 2. Let lenValue be the result of calling the Get
-            //    internal method of O with the argument "length".
-            // 3. Let len be ToUint32(lenValue).
-            var len = O.length >>> 0;
-
-            // 4. If len is 0, return -1.
-            if (len === 0) {
-                return -1;
-            }
-
-            // 5. If argument fromIndex was passed let n be
-            //    ToInteger(fromIndex); else let n be 0.
-            var n = +fromIndex || 0;
-
-            if (Math.abs(n) === Infinity) {
-                n = 0;
-            }
-
-            // 6. If n >= len, return -1.
-            if (n >= len) {
-                return -1;
-            }
-
-            // 7. If n >= 0, then Let k be n.
-            // 8. Else, n<0, Let k be len - abs(n).
-            //    If k is less than 0, then let k be 0.
-            k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-
-            // 9. Repeat, while k < len
-            while (k < len) {
-                // a. Let Pk be ToString(k).
-                //   This is implicit for LHS operands of the in operator
-                // b. Let kPresent be the result of calling the
-                //    HasProperty internal method of O with argument Pk.
-                //   This step can be combined with c
-                // c. If kPresent is true, then
-                //    i.  Let elementK be the result of calling the Get
-                //        internal method of O with the argument ToString(k).
-                //   ii.  Let same be the result of applying the
-                //        Strict Equality Comparison Algorithm to
-                //        searchElement and elementK.
-                //  iii.  If same is true, return k.
-                if (k in O && O[k] === searchElement) {
-                    return k;
-                }
-                k++;
-            }
-            return -1;
-        };
-    }
 }(window));
