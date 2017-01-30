1f70dc8cbe09e5b9cf6aa586bd44d38ce05a071d
Jonathan Richards <joniff@hotmail.com>
2017-01-30 10:39:15 +0000

    GMap css control

---
+++ src/Terratype.TestSite/App_Plugins/DocTypeGridEditor/Config/grid.editors.config.js
@@ -0,0 +1,16 @@
+﻿{
+    "name": "Doc Type",
+    "alias": "docType",
+    "view": "/App_Plugins/DocTypeGridEditor/Views/doctypegrideditor.html",
+    "render": "/App_Plugins/DocTypeGridEditor/Render/DocTypeGridEditor.cshtml",
+    "icon": "icon-item-arrangement",
+    "config": {
+        "allowedDocTypes": [],
+        "nameTemplate": "",
+        "enablePreview": true,
+        "viewPath": "/Views/Partials/Grid/Editors/DocTypeGridEditor/",
+        "previewViewPath": "/Views/Partials/Grid/Editors/DocTypeGridEditor/Previews/",
+        "previewCssFilePath": "",
+        "previewJsFilePath": ""
+    }
+}
