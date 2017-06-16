c9a7ba37f8fc9714940dd0900b833422ee43701c
Jonathan Richards <jrichards@rippleffect.com>
2017-06-16 14:46:30 +0100

    Added json creation example to test website

--- src/Terratype.TestSite/App_Start/TestProvider.cs
+++ src/Terratype.TestSite/App_Start/TestProvider.cs
@@ -1,8 +1,30 @@
 ﻿
+using Umbraco.Core;
+
 namespace Terratype.TestSite.App_Start
 {
     public class TestProvider
     {
+        /// <summary>
+        /// Create a content node in the root of the content tree
+        /// </summary>
+        /// <param name="nodeName">Name of the node</param>
+        /// <param name="latitude">Latitude of the map marker</param>
+        /// <param name="longitude">Longitude of the map marker</param>
+        /// <param name="label">The text to appear if the user clicks the map marker</param>
+        /// <returns></returns>
+        public bool CreateMapContent(string nodeName, double latitude, double longitude, string label)
+        {
+            var content = Umbraco.Core.ApplicationContext.Current.Services.ContentService.CreateContent(nodeName, -1, "mydoctype", 0);
+            var json = "{zoom: 5,label: {content: " + label + ",id: \"standard\",enable: true,editPosition: 0," +
+                "view: \"/App_Plugins/Terratype/views/label.standard.html?cache=1.0.12\",controller: \"terratype.label.standard\"}" +
+                "position: {id: \"WGS84\",datum: " + latitude.ToString(System.Globalization.CultureInfo.InvariantCulture) + "," +
+                longitude.ToString(System.Globalization.CultureInfo.InvariantCulture) + "}";
+            content.SetValue("mymap", json);
+            return ApplicationContext.Current.Services.ContentService.SaveAndPublishWithStatus(content).Success;
+        }
+
+
         public TestProvider()
         {
             foreach (System.Reflection.Assembly currAssembly in System.AppDomain.CurrentDomain.GetAssemblies())
