a31f19d658f42a160171f51ca1afbbb6dc0a8cd9
joniff <joniff@hotmail.com>
2018-07-09 14:08:08 +0100

    Released 1.0.19

--- src/Core/Terratype/Properties/AssemblyInfo.cs
+++ src/Core/Terratype/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Terratype")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core Umbraco package for Mapping services")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Terratype")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.19.0")]
+[assembly: AssemblyFileVersion("1.0.19.0")]
+[assembly: AssemblyInformationalVersion("1.0.19.0")]
